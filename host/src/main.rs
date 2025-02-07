795f2e19bef9adbf9d7569847829807cc00832f4
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-02-07 18:19:30 -0300

    Add real proof printing

--- host/src/main.rs
+++ host/src/main.rs
@@ -19,7 +19,7 @@ use clap::Parser;
 use erc20_methods::ERC20_GUEST_ELF;
 use host::verify_blob::{decode_blob_info, IVerifyBlob};
 use risc0_steel::{ethereum::EthEvmEnv, Commitment, Contract};
-use risc0_zkvm::{compute_image_id, default_executor, default_prover, ExecutorEnv, ProverOpts, VerifierContext};
+use risc0_zkvm::{compute_image_id, default_executor, default_prover, sha::Digestible, ExecutorEnv, ProverOpts, VerifierContext};
 use tokio_postgres::NoTls;
 use tracing_subscriber::EnvFilter;
 use url::Url;
@@ -27,7 +27,7 @@ use url::Url;
 /// Address of the deployed contract to call the function on (USDT contract on Sepolia).
 const CONTRACT: Address = address!("c551b009C1CE0b6efD691E23998AEFd4103680D3"); //TODO: Add the address of the deployed contract.
 /// Address of the caller.
-const CALLER: Address = address!("e706e60ab5Dc512C36A4646D719b889F398cbBcB");
+const CALLER: Address = address!("E90E12261CCb0F3F7976Ae611A29e84a6A85f424");
 
 /// Simple program to show the use of Ethereum contract data inside the guest.
 #[derive(Parser, Debug)]
@@ -40,9 +40,8 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
-    let imageid = compute_image_id(ERC20_GUEST_ELF)?;
-    println!("Image ID: {:?}", imageid);
+    let image_id = compute_image_id(ERC20_GUEST_ELF)?;
+    println!("Image id {:?}", image_id);
     let (client, connection) = tokio_postgres::connect(
         "host=localhost user=postgres password=notsecurepassword dbname=zksync_server_localhost_eigenda", 
         NoTls,
@@ -109,14 +108,38 @@ async fn main() -> Result<()> {
                 .build()
                 .unwrap();
             let exec = default_prover();
-            exec.prove_with_ctx(env,&VerifierContext::default(), ERC20_GUEST_ELF,&ProverOpts::succinct())
+            exec.prove_with_ctx(env,&VerifierContext::default(), ERC20_GUEST_ELF,&ProverOpts::groth16())
                 .context("failed to run executor")
         }).await??;
 
-        // The journal should be the ABI encoded commitment.
-        let commitment = Commitment::abi_decode(session_info.receipt.journal.as_ref(), true)
-            .context("failed to decode journal")?;
-        println!("{:?}", commitment);
+        let block_proof = match session_info.receipt.inner.groth16() {
+            Ok(inner) => {
+                // The SELECTOR is used to perform an extra check inside the groth16 verifier contract.
+                let mut selector = hex::encode(
+                    inner
+                        .verifier_parameters
+                        .as_bytes()
+                        .get(..4)
+                        .unwrap(),
+                );
+                let seal = hex::encode(inner.clone().seal);
+                selector.push_str(&seal);
+                hex::decode(selector).unwrap()
+            }
+            Err(_) => vec![0u8; 4],
+        };
+
+        let image_id: risc0_zkvm::sha::Digest = image_id.into();
+        let image_id = image_id.as_bytes().to_vec();
+
+        let journal_digest = Digestible::digest(&session_info.receipt.journal)
+            .as_bytes()
+            .to_vec();
+
+        println!("journal digest {:x?}", journal_digest);
+        println!("image id digest {:x?}", image_id);
+        println!("block proof {:x?}", block_proof);
+
     }
     Ok(())
 }
