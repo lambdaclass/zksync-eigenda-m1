9823aae4f82ad10102c27c2e2f7d3313b16ad375
Gianbelinche <gianluca.belinche@lambdaclass.com>
2025-03-07 18:46:29 -0300

    Initial integration in complete flow

--- proof-equivalence-methods/guest/src/main.rs
+++ proof-equivalence-methods/guest/src/main.rs
@@ -51,54 +51,16 @@ fn main() {
 
     let data: Vec<u8> = env::read();
     println!("data gotten");
-    let g1s: Vec<SerializableG1> = env::read();
-    println!("g1s gotten");
-    let mut g1: Vec<G1Affine> = vec![];
-    for g in g1s {
-        g1.push(g.g1);
-    }
-    //let g1: Vec<G1Affine> = g1s.into_iter().map(|x| G1Affine::deserialize_compressed_unchecked(&x[..]).unwrap()).collect();
-    println!("g1s converted");
-    let order: u32 = env::read();
-    println!("order read");
-
-    let srs = SRS{g1,order};
-
-    println!("env gotten");
 
     let blob = Blob::from_raw_data(&data);
 
+    let eval_commitment: SerializableG1 = env::read();
+
+    let proof: SerializableG1 = env::read();
+
     println!("blob done");
 
-    let mut kzg = KZG::new();
-
-    println!("kzg done");
-    kzg.calculate_and_store_roots_of_unity(blob.len().try_into().unwrap()).unwrap();
-    println!("roots");
-    
-    let x: Vec<u8> = vec![20, 153, 170, 133, 150, 17, 219, 215, 90, 29, 61, 41, 183, 105, 4, 139, 14, 161, 160, 7, 49, 89, 23, 57, 49, 52, 16, 175, 112, 57, 19, 50];
-    let y: Vec<u8> =  vec![47, 50, 235, 25, 170, 240, 84, 149, 189, 33, 211, 171, 1, 250, 141, 124, 116, 49, 37, 211, 193, 146, 250, 255, 63, 16, 117, 92, 28, 237, 120, 166];
-    
-    let x_fq = Fq::from(num_bigint::BigUint::from_bytes_be(&x));
-    println!("x done");
-    let y_fq =  Fq::from(num_bigint::BigUint::from_bytes_be(&y));
-    println!("y done");
-    
-    let commitment = G1Affine::new(x_fq, y_fq);
-    println!("com done");
-    let real_commitment = kzg.commit_coeff_form(&blob.to_polynomial_coeff_form(), &srs).unwrap();
-    println!("real done");
-    
-    assert!(commitment == real_commitment);
-
-    let eval_commitment = kzg.commit_eval_form(&blob.to_polynomial_eval_form(), &srs).unwrap();
-
-    println!("eval done");
-
-    let proof = kzg.compute_blob_proof(&blob, &eval_commitment, &srs).unwrap();
-    println!("proof done");
-
-    let verified = verify_blob_kzg_proof(&blob, &eval_commitment, &proof).unwrap();
+    let verified = verify_blob_kzg_proof(&blob, &eval_commitment.g1, &proof.g1).unwrap();
     
     println!("verified done");
     assert!(verified);
