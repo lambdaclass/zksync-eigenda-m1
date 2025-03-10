69eb590978f4c251c36f34377352e92c7f83247f
Gianbelinche <gianluca.belinche@lambdaclass.com>
2025-03-10 14:24:54 -0300

    Finish proof eq integration

--- host/src/proof_equivalence.rs
+++ host/src/proof_equivalence.rs
@@ -33,16 +33,6 @@ pub async fn run_proof_equivalence(
     commitment: G1Point,
     data: Vec<u8>,
 ) -> anyhow::Result<ProveInfo> {
-    //let content = std::fs::read_to_string("sample_data.txt").unwrap(); // todo add real blob
-
-    // Blob data BEFORE padding
-    /*let data: Vec<u8> = content
-        .split(',')
-        .map(|s| s.trim()) // Remove any leading/trailing spaces
-        .filter(|s| !s.is_empty()) // Ignore empty strings
-        .map(|s| s.parse::<u8>().expect("Invalid number")) // Parse as u8
-        .collect();*/
-
     let blob = Blob::from_raw_data(&data);
 
     let mut kzg = KZG::new();
@@ -51,28 +41,20 @@ pub async fn run_proof_equivalence(
 
     let x: [u8;32] = commitment.x.to_be_bytes();
     let y: [u8;32] = commitment.y.to_be_bytes();
-
-    //let x: Vec<u8> = vec![20, 153, 170, 133, 150, 17, 219, 215, 90, 29, 61, 41, 183, 105, 4, 139, 14, 161, 160, 7, 49, 89, 23, 57, 49, 52, 16, 175, 112, 57, 19, 50];
-    //let y: Vec<u8> =  vec![47, 50, 235, 25, 170, 240, 84, 149, 189, 33, 211, 171, 1, 250, 141, 124, 116, 49, 37, 211, 193, 146, 250, 255, 63, 16, 117, 92, 28, 237, 120, 166];
     
     let x_fq = Fq::from(num_bigint::BigUint::from_bytes_be(&x));
-    println!("x done");
     let y_fq =  Fq::from(num_bigint::BigUint::from_bytes_be(&y));
-    println!("y done");
     
     let commitment = G1Affine::new(x_fq, y_fq);
-    println!("com done");
     let real_commitment = kzg.commit_coeff_form(&blob.to_polynomial_coeff_form(), &srs).unwrap();
-    println!("real done");
-    
-    assert!(commitment == real_commitment);
 
+    if commitment != real_commitment {
+        return Err(anyhow::anyhow!("Commitments mismatched, given commitment: {:?}, real commitment: {:?}", commitment, real_commitment))
+    }
+    
     let eval_commitment = kzg.commit_eval_form(&blob.to_polynomial_eval_form(), &srs).unwrap();
 
-    println!("eval done");
-
     let proof = kzg.compute_blob_proof(&blob, &eval_commitment, &srs).unwrap();
-    println!("proof done");
 
     let serializable_eval = SerializableG1{g1: eval_commitment};
     let serializable_proof = SerializableG1{g1: proof};
@@ -91,7 +73,6 @@ pub async fn run_proof_equivalence(
         )
         .context("failed to run executor")
     }).await??;
-    println!("Finished run");
 
     Ok(session_info)
 
