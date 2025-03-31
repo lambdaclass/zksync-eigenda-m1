39393a23c3fb7f5d2c7ec1b549a2a1f4e7e7b742
gianbelinche <gianluca.belinche@lambdaclass.com>
2025-03-31 16:16:54 -0300

    Add ownable and proxy to registry

--- contracts/src/EigenDARegistry.sol
+++ contracts/src/EigenDARegistry.sol
@@ -1,22 +1,36 @@
 // ViewBypass.sol
 pragma solidity ^0.8.20;
 
+import {UUPSUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {Ownable2StepUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/access/Ownable2StepUpgradeable.sol";
+import {Initializable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+
+
 interface IRiscZeroVerifier {
     function verify(bytes calldata seal, bytes32 imageId, bytes32 journalDigest) external view;
 }
 
 // Sends proofs to verify to the Risc0 groth16 verifier, storing whether they are verified, along with the hash of the blob
-contract EigenDARegistry {
+contract EigenDARegistry is Initializable, UUPSUpgradeable, Ownable2StepUpgradeable {
+    
     IRiscZeroVerifier public risc0verifier;
     mapping (bytes => bool) public finishedBatches;
     mapping (bytes => bool) public verifiedBatches;
     mapping (bytes => bytes32) public hashes;
 
-    constructor(address _risc0verifier) {
+     /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _risc0verifier, address _owner) public initializer {
+        __Ownable2Step_init();
+        __UUPSUpgradeable_init();
+        _transferOwnership(_owner);
         risc0verifier = IRiscZeroVerifier(_risc0verifier);
     }
 
-    function verify(bytes calldata seal, bytes32 imageId, bytes32 journalDigest, bytes32 eigendaHash, bytes calldata inclusion_data) public {
+    function verify(bytes calldata seal, bytes32 imageId, bytes32 journalDigest, bytes32 eigendaHash, bytes calldata inclusion_data) public onlyOwner {
         try risc0verifier.verify(seal, imageId, journalDigest) {
             finishedBatches[inclusion_data] = true;
             verifiedBatches[inclusion_data] = true;
@@ -30,4 +44,7 @@ contract EigenDARegistry {
     function isVerified(bytes calldata inclusion_data) external view returns (bool, bytes32) {
         return (verifiedBatches[inclusion_data], hashes[inclusion_data]);
     }
+
+    /// @dev Restricts upgrade permission to the contract owner
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
