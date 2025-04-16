268e67cdcec747be37b67c2ff232fd5e490beebc
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-04-16 12:41:20 -0300

    Redo proof of equivalence

--- common/src/output.rs
+++ common/src/output.rs
@@ -1,7 +1,11 @@
 use serde::{Deserialize, Serialize};
 
+use crate::blob_info::BlobInfo;
+
 #[derive(Serialize, Deserialize)]
 pub struct Output {
     pub hash: Vec<u8>,
     pub env_commitment: Vec<u8>,
+    pub blob_info: BlobInfo,
+    pub proof: Vec<u8>,
 }
