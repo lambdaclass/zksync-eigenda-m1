bc483777555ba93b1c13c89e331f3e46614e357d
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-06-27 15:55:01 -0300

    Remove inclusion data from output

--- common/src/output.rs
+++ common/src/output.rs
@@ -3,7 +3,6 @@ use ethabi::Token;
 pub struct Output {
     pub hash: Vec<u8>,
     pub env_commitment: Vec<u8>,
-    pub inclusion_data: Vec<u8>,
     pub proof: Vec<u8>,
 }
 
@@ -14,7 +13,6 @@ impl Output {
             vec![
                 Token::FixedBytes(self.hash),
                 Token::Bytes(self.env_commitment),
-                Token::Bytes(self.inclusion_data),
                 Token::Bytes(self.proof)
             ]
         )])
